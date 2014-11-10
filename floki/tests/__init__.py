d16e921a80e2dcdcb0455d4766dc8b64abb60608
Mateus Kern <kern@mateuskern.com>
2014-11-10 17:01:29 -0200

    write test for get_list

--- floki/tests/__init__.py
+++ floki/tests/__init__.py
@@ -23,7 +23,30 @@ class functions_with_return(unittest.TestCase):
                          'debian-amd64.vmwarevm/debian-amd64.vmx')
 
     def test_get_list(self):
-        pass
+        result = {'cache01': '/Volumes/Hulk/VirtualMachines/skaro/' +
+                             'development/cache01.vmwarevm/cache01.vmx',
+                  'cache02': '/Volumes/Hulk/VirtualMachines/skaro/' +
+                             'development/cache02.vmwarevm/cache02.vmx'}
+        self.assertEqual(self.machine.get_list('development', ['alt-servers']),
+                         result)
+
+    def test_get_list_with_all_groups(self):
+        groups = ['all']
+        result = {'cache01': '/Volumes/Hulk/VirtualMachines/skaro/' +
+                             'development/cache01.vmwarevm/cache01.vmx',
+                  'cache02': '/Volumes/Hulk/VirtualMachines/skaro/' +
+                             'development/cache02.vmwarevm/cache02.vmx',
+                  'web04': '/Volumes/Hulk/VirtualMachines/templates/' +
+                           'debian-amd64.vmwarevm/debian-amd64.vmx',
+                  'web03': '/Volumes/Hulk/VirtualMachines/skaro/' +
+                           'development/web03.vmwarevm/web03.vmx',
+                  'web01': '/Volumes/Hulk/VirtualMachines/skaro/' +
+                           'development/web01.vmwarevm/web01.vmx',
+                  'db02': '/Volumes/Hulk/VirtualMachines/skaro/' +
+                          'development/db02.vmwarevm/db02.vmx',
+                  'db01': '/Volumes/Hulk/VirtualMachines/skaro/' +
+                          'development/db01.vmwarevm/db01.vmx'}
+        self.assertEqual(self.machine.get_list('development', groups), result)
 
 if __name__ == "__main__":
     unittest.main()
