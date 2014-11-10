d3a6311d22e5ad447870607f1af711aad5e43eec
Mateus Kern <kern@mateuskern.com>
2014-11-10 16:20:35 -0200

    write test for get_vmx_path

--- floki/tests/__init__.py
+++ floki/tests/__init__.py
@@ -6,6 +6,14 @@ class functions_with_return(unittest.TestCase):
 
     def setUp(self):
         self.config = 0
+        self.machine = Machines('config.yml')
+
+    def test_get_vmx_path(self):
+        self.assertEqual(self.machine.get_vmx_path('development',
+                                                   'main-servers',
+                                                   'web01'),
+                         '/Volumes/Hulk/VirtualMachines/skaro/development/' +
+                         'web01.vmwarevm/web01.vmx')
 
     def test_get_list(self):
         pass
