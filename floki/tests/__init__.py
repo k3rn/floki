b284f25c9249158ecc571da193374ee5fe8f6b44
Mateus Kern <kern@mateuskern.com>
2014-11-10 16:25:50 -0200

    write test for get_vmx_path, but with the path set in the config file

--- floki/tests/__init__.py
+++ floki/tests/__init__.py
@@ -15,6 +15,13 @@ class functions_with_return(unittest.TestCase):
                          '/Volumes/Hulk/VirtualMachines/skaro/development/' +
                          'web01.vmwarevm/web01.vmx')
 
+    def test_get_vmx_path_with_path_set(self):
+        self.assertEqual(self.machine.get_vmx_path('development',
+                                                   'main-servers',
+                                                   'web04'),
+                         '/Volumes/Hulk/VirtualMachines/templates/' +
+                         'debian-amd64.vmwarevm/debian-amd64.vmx')
+
     def test_get_list(self):
         pass
 
